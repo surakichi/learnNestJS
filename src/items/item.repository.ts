a5740aa68c370ff9ea96ab4b96003397c7fb5f1a
fukumoto.katsuhisa <fukumoto.katsuhisa.6g4@jp.nssol.nipponsteel.com>
2022-05-15 19:38:29 +0900

    Use JWT Access token when update item

--- src/items/item.repository.ts
+++ src/items/item.repository.ts
@@ -1,14 +1,12 @@
 import { Item } from 'src/entities/item.entity';
+import { User } from 'src/entities/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemStatus } from './item-status.enum';
 
 @EntityRepository(Item)
 export class ItemRepository extends Repository<Item> {
-  async createItem(
-    createItemDto: CreateItemDto,
-    userId: string,
-  ): Promise<Item> {
+  async createItem(createItemDto: CreateItemDto, user: User): Promise<Item> {
     const { name, price, description } = createItemDto;
     const item = this.create({
       name,
@@ -17,7 +15,7 @@ export class ItemRepository extends Repository<Item> {
       status: ItemStatus.ON_SALE,
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
-      userId,
+      user,
     });
     await this.save(item);
 
